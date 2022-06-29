298c23467cebdb0319047f0f7309bf7f3f44cf72
Kaddijatou Baldeh <baldehk@spu.edu>
2022-06-28 20:21:33 -0700

    Implementation of homework1

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -13,60 +13,60 @@ using std::endl;
 using std::shuffle;
 
 /**
- * Declaration of sub-functions to use as helpers functions in main
+ * Declaration of sub-functions to use as helpers in main
  */
 string Print(const string& s);
 bool IsValidString(const string& s);
 
 /**
  * Main function takes in nine digit string from console and shuffles the string
- * @param argc command line argument count
+ * @param argc command line argument count determines amount of shuffles
  * @param argv command line argument array pointer
  * @return 0
  */
 int main(int argc, char *argv[]) {
     int totalShuffle;
     if (argc > 1){
-        // check if the argument string is a digit and not null
+        // check if the argument string is a digit and not the null terminating character
         for (int i = 0; argv[1][i] != '\0'; i++){
             if (isdigit(argv[1][i])) {
-                //set total shuffle to the value if its a digit
+                //set total shuffle to the value converting string to int
                 totalShuffle = stoi(argv[1]);
             }
         }
     } else{
         totalShuffle = 1;
     }
-
     // get user input for string of nine digits
     string digits;
     cout << "Please input 9 digits to be randomly shuffled:" << endl;
     cin >> digits;
-    //check if the string length is 9 and all digits
+    // check if the string length is 9 and all digits
     bool ok = IsValidString(digits);
-
-    const char* value[10] = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth","ninth", "tenth"};
-
-    if(ok){// if string is valid
+    // if string is valid
+    if(ok){
+        // vales to represent each iteration
+        const char* value[10] = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth","ninth", "tenth"};
+        //loop to shuffle and print to console
         for(int i = 0; i < totalShuffle; i++){
             // shuffle the string using shuffle method of algorithm
             shuffle(digits.begin(),digits.end(), std::mt19937(std::random_device()()));
 
-            //break down the shuffled string into 3
+            // break down the shuffled string into 3
             string firstLine, secondLine, thirdLine;
             firstLine = digits.substr(0,3);
             secondLine= digits.substr(3,3);
             thirdLine = digits.substr(6,3);
 
-            //header based on iteration of shuffle
+            // header based on iteration of shuffle
             cout << "The " << value[i] << " shuffled output is:" << endl;
 
-            //output shuffled string as 3x3
+            // output shuffled string as 3x3
             cout << "+-------+ " << endl;
             cout << Print(firstLine) << endl;
             cout << Print(secondLine) << endl;
             cout << Print(thirdLine) << endl;
-            cout << "+-------+ " << endl;
+            cout << "+-------+ " << endl << endl;
         }
     }else{// if string contains other characters instead of numbers
         cout << "Invalid input" << endl;
